10f0283be04b1617802db30b88d18fb3485ccef6
chaithanya manas reddy <manas@chaithanyasair7.nwmissouri.edu>
2018-02-15 15:38:00 -0600

    Initial Commit

---
+++ Catalog/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Catalog
+//
+//  Created by chaithanya manas reddy on 15/02/18.
+//  Copyright © 2018 Chaithanya Manas Reddy Matta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Catalog/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Catalog
+//
+//  Created by chaithanya manas reddy on 15/02/18.
+//  Copyright © 2018 Chaithanya Manas Reddy Matta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
