ea8427582dd32e6cbedc06895bea5d16a3d287c9
sai kumar uppala <saikumaruppala@Veeras-MacBook-Air.local>
2018-02-16 21:51:39 -0600

    Sai Kumar Uppala Created Table View with sample Data

--- Catalog/ListViewController.swift
+++ Catalog/ListViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    let List_Array = ["Eggs","Milk","Potatos","Nutella","Water Bottles"]
+    let Quantity = ["60","2","2","1","40"]
+    let Units = ["pieces","Gallons","lbs","piece","pieces"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return List_Array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToBuyItems")
+        cell?.textLabel?.text = List_Array[indexPath.row]
+        cell?.detailTextLabel?.text=Quantity[indexPath.row]+" "+Units[indexPath.row]
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
