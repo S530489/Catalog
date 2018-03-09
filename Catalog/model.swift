43083c26312467a4fd0eaa15f60af2919fad5c04
chaithanya manas reddy <manas@chaithanyas-macbook-air-7.local>
2018-03-09 17:11:15 -0600

    chaithanya Manas Reddy Matta :  Commit for Login page layout

---
+++ Catalog/model.swift
@@ -0,0 +1,10 @@
+//
+//  model.swift
+//  Catalog
+//
+//  Created by chaithanya manas reddy on 09/03/18.
+//  Copyright © 2018 Chaithanya Manas Reddy Matta. All rights reserved.
+//
+
+import Foundation
+
