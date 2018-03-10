f2ef7f5781c99ca35f9ab4e7a377f4492c8da1b1
Chandra Mouli <kchmouli@Chandras-iMac.local>
2018-03-09 22:45:37 -0600

    Chandra Mouli Kantipudi - Issue resolved in ListTableViewController

--- ListTableViewController.swift
+++ ListTableViewController.swift
@@ -29,23 +29,23 @@ class ListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return AppDelegate.myModel.items.count
+        return 0
     }
 
-    
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListOfItems", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = AppDelegate.myModel.items[indexPath.row].name
+
         return cell
     }
- 
+    */
 
     /*
     // Override to support conditional editing of the table view.
