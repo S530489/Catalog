01be149a7ba5b36865e3e4b0cc8a72072c5b679a
Student <student@jesses-macbook-air.local>
2018-04-18 19:48:26 -0500

    Chandra Mouli Kantipudi : Fixed the logic in the category View controller

--- Catalog/ListTableViewController.swift
+++ Catalog/ListTableViewController.swift
@@ -42,23 +42,27 @@ class ListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchItems()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+//    override func viewDidAppear(_ animated: Bool) {
+//        fetchItems()
+//        getStoreItems()
+//    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     override func viewWillAppear(_ animated: Bool) {
-        
+        fetchItems()
         getStoreItems()
         
     }
+   
 
     // MARK: - Table view data source
 
